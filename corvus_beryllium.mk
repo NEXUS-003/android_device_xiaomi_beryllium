1b5d6322be392b5214d6c37b88a4466569f33097
NEXUS-003 <shuklakantmani123@gmail.com>
2022-05-01 11:37:33 +0530

    beryllium: CorvusOS bringup

--- corvus_beryllium.mk
+++ corvus_beryllium.mk
@@ -6,11 +6,14 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Corvus stuff.
+$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
+
+# Corvus Maintainer
+CORVUS_MAINTAINER=𝙉𝙚𝙭𝙪𝙨
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := corvus_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
